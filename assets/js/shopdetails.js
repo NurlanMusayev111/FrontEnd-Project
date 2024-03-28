bdeb334ae7c7e647b254542b1762611973ae0f5f
nurlanmusayev <nurlan.musayev.2002@inbox.ru>
2024-03-28 17:46:32 +0400

    tab menular duzeldi

--- assets/js/shopdetails.js
+++ assets/js/shopdetails.js
@@ -8,20 +8,38 @@ let reviewBtn = document.querySelector(".button-review");
 
 let contentDesc = document.querySelector(".content-description");
 let contentInfo = document.querySelector(".content-addinfo");
+let contentReview = document.querySelector("content-review");
+
+
 
 
 descBtn.addEventListener("click",function(){
     infoBtn.classList.remove("active");
     descBtn.classList.add("active")
+    reviewBtn.classList.remove("active")
 
     contentInfo.classList.add("d-none");
+    contentReview.classList.add("d-none")
     contentDesc.classList.remove("d-none");
+
 })
 
 infoBtn.addEventListener("click",function(){
     descBtn.classList.remove("active");
     infoBtn.classList.add("active");
+    reviewBtn.classList.remove("active");
 
     contentDesc.classList.add("d-none");
-    contentInfo.classList.remove("d-none")
+    contentInfo.classList.remove("d-none");
+    contentReview.classList.add("d-none")
+})
+
+reviewBtn.addEventListener("click",function(){
+    reviewBtn.classList.add("active");
+    infoBtn.classList.remove("active");
+    descBtn.classList.remove("active");
+
+    contentReview.classList.remove("d-none");
+    contentDesc.classList.add("d-none");
+    contentInfo.classList.add("d-none");    
 })
