f516147ec412135416c6b51bac9febec154ccc34
nurlanmusayev <nurlan.musayev.2002@inbox.ru>
2024-03-29 00:15:51 +0400

    shop details sehifesi tam hazirdi

--- assets/js/shopdetails.js
+++ assets/js/shopdetails.js
@@ -8,7 +8,7 @@ let reviewBtn = document.querySelector(".button-review");
 
 let contentDesc = document.querySelector(".content-description");
 let contentInfo = document.querySelector(".content-addinfo");
-let contentReview = document.querySelector("content-review");
+let contentReview = document.querySelector(".content-review");
 
 
 
@@ -18,9 +18,9 @@ descBtn.addEventListener("click",function(){
     descBtn.classList.add("active")
     reviewBtn.classList.remove("active")
 
+    contentDesc.classList.remove("d-none");
     contentInfo.classList.add("d-none");
     contentReview.classList.add("d-none")
-    contentDesc.classList.remove("d-none");
 
 })
 
@@ -39,7 +39,7 @@ reviewBtn.addEventListener("click",function(){
     infoBtn.classList.remove("active");
     descBtn.classList.remove("active");
 
-    contentReview.classList.remove("d-none");
     contentDesc.classList.add("d-none");
-    contentInfo.classList.add("d-none");    
+    contentInfo.classList.add("d-none"); 
+    contentReview.classList.remove("d-none");   
 })
